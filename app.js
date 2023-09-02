1255b77594b80c5b92201139092ffe61d3443c9a
Gilberto <ingenierobeto25@gmail.com>
2023-09-02 14:34:01 -0400

    Slider and text, ok

--- app.js
+++ app.js
@@ -1,32 +1,51 @@
-$(document).ready(function () {
-  
-  var image = $(".slides img");
-  var imageArray = [];
-  var imagePosition = 0;
 
- image.each(function () {
+var image = $(".slides img");
+var paragraph = $(".slides .buttonLieu");
+var imageArray = [];
+var index = 0;
+var paragraphArray = [];
+var paragraphIndex = 0;
+
+/*
+var paragraph = document.querySelectorAll(".buttonLieu");
+//console.log(paragraph);
+//alert(paragraph);
+
+for(var i=0; i<paragraph.length; i++){
+    console.log(paragraph[i].innerHTML);
+    alert(paragraph[i].innerHTML);
+}*/
+
+
+image.each(function () {
     imageArray.push($(this).attr("src"));
-  }); 
+}); 
 
-  $(".prev").click(function () {
-  
-    imagePosition = imagePosition - 1;
-    if (imagePosition < 0) {
-      imagePosition = imageArray.length - 1;
-    }
-    image.attr("src", imageArray[imagePosition]);
-  
-  });
+paragraph.each(function () {
+    paragraphArray.push($(this));
+}); 
 
-  $(".next").click(function () {
-   
-    imagePosition = imagePosition + 1;
-    if (imagePosition > imageArray.length - 1) {
-      imagePosition = 0;
-    }
-    image.attr("src", imageArray[imagePosition]);
-  
-  });
 
-  
+$(".prev").click(function () {
+
+    index = index - 1;
+    if(index < 0){
+        index = imageArray.length - 1;
+    };
+    $("#imageSlider").attr("src", imageArray[index]);
+    $("#textSlider")[0].innerHTML = paragraphArray[index][0].innerHTML ;
+
 });
+
+$(".next").click(function () {
+
+index = index + 1;
+if (index > imageArray.length - 1) {
+    index = 0;
+}
+image.attr("src", imageArray[index]);
+
+});
+
+
+
